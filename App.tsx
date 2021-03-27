d7be5313b99e40b6347d9c0be74a10382ea1ac27
Harry Pham <harrypham@swatmobile.io>
2021-03-27 16:54:47 +0800

    Make barebone project

--- App.tsx
+++ App.tsx
@@ -1,19 +1,10 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import * as Notifications from 'expo-notifications'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { NavigationContainer } from '@react-navigation/native'
 import { ThemeProvider } from 'react-native-elements'
 
 import HomeScreen from './src/screens/HomeScreen'
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-})
 
 export default function App() {
   return (
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,25 +1,11 @@
 import * as React from 'react'
 import { Text, View } from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import BoyScreen from './BoyScreen'
-import GirlScreen from './GirlScreen'
-
-const Tab = createBottomTabNavigator()
 
 const HomeScreen: React.FC = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Gấu cái"
-        options={{ tabBarIcon: () => <Text>👧</Text> }}
-        component={GirlScreen}
-      />
-      <Tab.Screen
-        name="Gấu đực"
-        options={{ tabBarIcon: () => <Text>👦</Text> }}
-        component={BoyScreen}
-      />
-    </Tab.Navigator>
+    <View>
+      <Text>Hello World</Text>
+    </View>
   )
 }
 
